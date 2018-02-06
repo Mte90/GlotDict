1c424451c111ed3596c3c7367edba2a691635060
Daniele Scasciafratte <mte90net@gmail.com>
2018-02-06 22:40:54 +0100

    fix #130

--- js/glotdict-validation.js
+++ js/glotdict-validation.js
@@ -1,18 +1,18 @@
-/* global key, glotdict_version, $gp, $gp_editor_options */
-
 /**
  * Run the review
  * 
  * @returns void
  */
 function gd_run_review() {
-  jQuery('tr.preview').each(function () {
-	var $preview = jQuery(this);
-	var editor = '#editor-' + $preview.attr('row') + ':not(.untranslated)';
-	var howmany = gd_validate('', editor);
-	if (howmany > 0) {
-	  $preview.find('.checkbox').css({'background': 'red'});
-	}
+  jQuery('tr.preview').each(function() {
+    var $preview = jQuery(this);
+    var editor = '#editor-' + $preview.attr('row') + ':not(.untranslated)';
+    var howmany = gd_validate('', editor);
+    if (howmany > 0) {
+      $preview.find('.checkbox').css({
+        'background': 'red'
+      });
+    }
   });
 }
 
@@ -27,35 +27,35 @@ function gd_run_review() {
 function gd_search_glossary_on_translation(e, selector) {
   var howmany = 0;
   if (gd_get_setting('no_glossary_term_check')) {
-	return howmany;
+    return howmany;
   }
   var discard = gd_get_discard_link(selector);
-  jQuery(selector).each(function () {
-	var $editor = jQuery(this);
-	var translation = jQuery('textarea', $editor).val();
-	jQuery('.glossary-word', $editor).each(function () {
-	  var translations = jQuery(this).data('translations');
-	  var reset = '';
-	  var term = jQuery(this).html();
-	  jQuery(translations).each(function (index) {
-		if (translations[index].translation === 'N/A') {
-		  return true;
-		}
-		if (translation.search(new RegExp(translations[index].translation, 'gi')) === -1) {
-		  reset = reset + '"<b>' + translations[index].translation + '</b>", ';
-		} else {
-		  reset = '';
-		  return false;
-		}
-	  });
-	  if (reset !== '') {
-		howmany++;
-		jQuery('.textareas', $editor).prepend(gd_get_warning('The translation is missing the suggested translation ' + reset + 'for "<i>' + term + '</i>"', discard));
-	  }
-	});
+  jQuery(selector).each(function() {
+    var $editor = jQuery(this);
+    var translation = jQuery('textarea', $editor).val();
+    jQuery('.glossary-word', $editor).each(function() {
+      var translations = jQuery(this).data('translations');
+      var reset = '';
+      var term = jQuery(this).html();
+      jQuery(translations).each(function(index) {
+        if (translations[index].translation === 'N/A') {
+          return true;
+        }
+        if (translation.search(new RegExp(translations[index].translation, 'gi')) === -1) {
+          reset = reset + '"<b>' + translations[index].translation + '</b>", ';
+        } else {
+          reset = '';
+          return false;
+        }
+      });
+      if (reset !== '') {
+        howmany++;
+        jQuery('.textareas', $editor).prepend(gd_get_warning('The translation is missing the suggested translation ' + reset + 'for "<i>' + term + '</i>"', discard));
+      }
+    });
   });
   if (howmany !== 0) {
-	gd_stoppropagation(e);
+    gd_stoppropagation(e);
   }
   return howmany;
 }
@@ -65,74 +65,74 @@ function gd_search_glossary_on_translation(e, selector) {
  * 
  * @param {object} e The event.
  * @param {string} selector The selector.
- * 
+ *
  * @returns void
  */
 function gd_validate(e, selector) {
   var howmany = 0;
   if (typeof jQuery(selector).data('discard') === 'undefined') {
-	jQuery('.gd-warning', selector).remove();
-	howmany += jQuery('.warning:not(.gd-warning)', selector).length;
-	howmany += gd_search_glossary_on_translation(e, selector);
-	var newtext = jQuery('textarea', selector).val();
-	var discard = gd_get_discard_link(selector);
-	if (typeof newtext === 'undefined' || newtext === '') {
-	  jQuery('.textareas', selector).prepend(gd_get_warning('The translation seems empty!', discard));
-	  howmany++;
-	  return;
-	}
-	var originaltext = jQuery('.original', selector).text();
-	var lastcharoriginaltext = originaltext.slice(-1);
-	var firstcharoriginaltext = originaltext.charAt(0);
-	var hellipseoriginaltext = originaltext.slice(-3) === '...';
-	var lastcharnewtext = newtext.slice(-1);
-	var firstcharnewtext = newtext.charAt(0);
-	var last_dot = [';', '.', '!', ':', '、', '。', '؟', '？', '！'];
-	if (hellipseoriginaltext) {
-	  if (!gd_get_setting('no_final_dot')) {
-		if (jQuery('textarea', selector).val().slice(-3) === '...' || lastcharnewtext !== ';' && lastcharnewtext !== '.') {
-		  jQuery('.textareas', selector).prepend(gd_get_warning('The translation contains a final <b>...</b> that should be translated as <b><code>&amp;hellip;</code></b>', discard));
-		  howmany++;
-		}
-	  }
-	} else {
-	  if (!gd_get_setting('no_final_other_dots')) {
-		if (jQuery.inArray(lastcharoriginaltext, last_dot) > 0 && jQuery.inArray(lastcharnewtext, last_dot) === -1) {
-		  jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an ending <b>.</b> or <b>?</b> or <b>!</b>', discard));
-		  howmany++;
-		}
-	  }
-	}
-	if (!gd_get_setting('no_initial_uppercase')) {
-	  if (gd_is_uppercase(firstcharoriginaltext) && !gd_is_uppercase(firstcharnewtext)) {
-		jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an initial uppercase letter for "<i>' + firstcharnewtext + '</i>"', discard));
-		howmany++;
-	  }
-	}
-	if (!gd_get_setting('no_trailing_space')) {
-	  if ( (lastcharoriginaltext === ' ' && lastcharnewtext !== ' ' ) || (lastcharoriginaltext === ' ' && lastcharnewtext !== ' ' ) ) {
-	    jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an ending space or non-breaking space.', discard));
-	    howmany++;
-	  }
-	}
-	if (gd_get_setting('curly_apostrophe_warning')) {
-	  if (newtext.indexOf("'") > -1) {
-		jQuery('.textareas', selector).prepend(gd_get_warning('The translation is using straight apostrophes instead of curly ones. Please check them.', discard));
-		howmany++;
-	  }
-	}
+    jQuery('.gd-warning', selector).remove();
+    howmany += jQuery('.warning:not(.gd-warning):has(> a.discard-warning)', selector).length;
+    howmany += gd_search_glossary_on_translation(e, selector);
+    var newtext = jQuery('textarea', selector).val();
+    var discard = gd_get_discard_link(selector);
+    if (typeof newtext === 'undefined' || newtext === '') {
+      jQuery('.textareas', selector).prepend(gd_get_warning('The translation seems empty!', discard));
+      howmany++;
+      return;
+    }
+    var originaltext = jQuery('.original', selector).text();
+    var lastcharoriginaltext = originaltext.slice(-1);
+    var firstcharoriginaltext = originaltext.charAt(0);
+    var hellipseoriginaltext = originaltext.slice(-3) === '...';
+    var lastcharnewtext = newtext.slice(-1);
+    var firstcharnewtext = newtext.charAt(0);
+    var last_dot = [';', '.', '!', ':', '、', '。', '؟', '？', '！'];
+    if (hellipseoriginaltext) {
+      if (!gd_get_setting('no_final_dot')) {
+        if (jQuery('textarea', selector).val().slice(-3) === '...' || lastcharnewtext !== ';' && lastcharnewtext !== '.') {
+          jQuery('.textareas', selector).prepend(gd_get_warning('The translation contains a final <b>...</b> that should be translated as <b><code>&amp;hellip;</code></b>', discard));
+          howmany++;
+        }
+      }
+    } else {
+      if (!gd_get_setting('no_final_other_dots')) {
+        if (jQuery.inArray(lastcharoriginaltext, last_dot) > 0 && jQuery.inArray(lastcharnewtext, last_dot) === -1) {
+          jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an ending <b>.</b> or <b>?</b> or <b>!</b>', discard));
+          howmany++;
+        }
+      }
+    }
+    if (!gd_get_setting('no_initial_uppercase')) {
+      if (gd_is_uppercase(firstcharoriginaltext) && !gd_is_uppercase(firstcharnewtext)) {
+        jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an initial uppercase letter for "<i>' + firstcharnewtext + '</i>"', discard));
+        howmany++;
+      }
+    }
+    if (!gd_get_setting('no_trailing_space')) {
+      if ((lastcharoriginaltext === ' ' && lastcharnewtext !== ' ') || (lastcharoriginaltext === ' ' && lastcharnewtext !== ' ')) {
+        jQuery('.textareas', selector).prepend(gd_get_warning('The translation is missing an ending space or non-breaking space.', discard));
+        howmany++;
+      }
+    }
+    if (gd_get_setting('curly_apostrophe_warning')) {
+      if (newtext.indexOf("'") > -1) {
+        jQuery('.textareas', selector).prepend(gd_get_warning('The translation is using straight apostrophes instead of curly ones. Please check them.', discard));
+        howmany++;
+      }
+    }
   }
   if (howmany !== 0) {
-	gd_stoppropagation(e);
+    gd_stoppropagation(e);
   }
   return howmany;
 }
 
 function gd_validate_visible(e) {
-  var selector = '.editor:visible:not(.untranslated)';
+  var selector = '.editor:visible';
   var howmany = gd_validate(e, selector);
-  if(typeof howmany !== "undefined" && howmany !== 0) {
-	alert('You need to close the warning to approve the new string!');
+  if (typeof howmany !== "undefined" && howmany !== 0) {
+    alert('You need to close the warning to approve the new string!');
   }
 }
 
