512b1aae97c3ad1b7e014bbad0d1ba181c7e3b4a
Garrett Hyder <garrett@eclipsecreative.ca>
2018-07-10 09:47:23 -0700

    Updated String Count to precede the links and to make word count visible always

--- js/glotdict-meta.js
+++ js/glotdict-meta.js
@@ -1,5 +1,5 @@
 function gd_add_meta() {
-  jQuery('#translations tr.editor').each(function() { 
+  jQuery('#translations tr.editor').each(function() {
     gd_add_string_counts(this);
   } );
 }
@@ -8,7 +8,7 @@ function gd_add_string_counts(row) {
     if ( jQuery(row).find('.meta .gd-counts').length ) {
       jQuery(row).find('.meta .gd-counts').remove();
     }
-    jQuery(row).find('.meta').append('<div class="gd-counts"><h3>String Counts</h3></div>');
+    jQuery(row).find('.meta dl:last-of-type').before('<div class="gd-counts"></div>');
 
     gd_add_original_count(row);
 
@@ -27,27 +27,27 @@ function gd_add_original_count(row) {
     var originalCharacterCount = jQuery(row).find('.original').text().length;
     var originalWords = jQuery(row).find('.original').text().split(' ');
     var originalWordCount = originalWords.length;
-    jQuery(row).find('.gd-counts').append('<dl class="original-count"><dt>Original:</dt><dd><abbr title="' + originalWordCount + ' Words">' + originalCharacterCount + ' Characters</abbr></dd></dl>');
+    jQuery(row).find('.gd-counts').append('<dl class="original-count"><dt>Original String:</dt><dd><span class="characters">' + originalCharacterCount + ' Characters</span> (<span class="words">' + originalWordCount + ' Words</span>)</dd></dl>');
 }
 
 function gd_add_translation_count(row) {
     var translationCharacterCount = jQuery(row).find('.translation').text().length;
     var translationWords = jQuery(row).find('.original').text().split(' ');
     var translationWordCount = translationWords.length;
-    jQuery(row).find('.gd-counts').append('<dl class="translated-count"><dt>Translated</dt><dd><abbr title="' + translationWordCount + ' Words">' + translationCharacterCount + ' Characters</abbr></dd></dl>'); 
+    jQuery(row).find('.gd-counts').append('<dl class="translated-count"><dt>Translated String:</dt><dd><span class="characters">' + translationCharacterCount + ' Characters</span> (<span class="words">' + translationWordCount + ' Words</span>)</dd></dl>');
 }
 
 function gd_add_current_count(row) {
     var currentCharacterCount = jQuery(row).find('.textareas textarea.foreign-text').val().length;
     var currentWords = jQuery(row).find('.textareas textarea.foreign-text').val().split(' ');
     var currentWordCount = currentWords.length;
-    jQuery(row).find('.gd-counts').append('<dl class="current-count"><dt>Current</dt><dd><abbr title="' + currentWordCount + ' Words">' + currentCharacterCount + ' Characters</abbr></dd></dl>');
+    jQuery(row).find('.gd-counts').append('<dl class="current-count"><dt>Current String:</dt><dd><span class="characters">' + currentCharacterCount + ' Characters</span> (<span class="words">' + currentWordCount + ' Words</span>)</dl>');
 }
 
 function gd_update_current_count(row, textarea) {
     var currentCharacterCount = jQuery(textarea).val().length;
     var currentWords = jQuery(row).find('.textareas textarea.foreign-text').val().split(' ');
     var currentWordCount = currentWords.length;
-    jQuery(row).find('.gd-counts .current-count dd abbr').text(currentCharacterCount + ' Characters');
-    jQuery(row).find('.gd-counts .current-count dd abbr').attr('title', currentWordCount + ' Words');
+    jQuery(row).find('.gd-counts .current-count dd .characters').text(currentCharacterCount + ' Characters');
+    jQuery(row).find('.gd-counts .current-count dd .words').text(currentWordCount + ' Words');
 }
