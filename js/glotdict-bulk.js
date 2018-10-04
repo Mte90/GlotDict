447edbd3aa4abfdb5c9739cc0823043f35105a60
Karen <kaskar2008@mail.ru>
2018-10-04 19:41:36 +0300

    Update glotdict-bulk.js

--- js/glotdict-bulk.js
+++ js/glotdict-bulk.js
@@ -6,7 +6,7 @@ jQuery('.bulk-action').append(jQuery('<option>', {
 
 jQuery('.bulk-actions').on('click', '.button', function(e) {
   if (jQuery('.bulk-action option:selected').val() === 'copy-from-original') {
-      jQuery('th.checkbox input:checked').each(function() {
+    jQuery('th.checkbox input:checked').each(function() {
 
       // List of selectors to ignore
       var ignoreList = [
