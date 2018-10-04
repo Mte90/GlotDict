1971f3d9f769175f8e93818358858a26de6ff0fe
Karen <kaskar2008@mail.ru>
2018-10-04 19:39:36 +0300

    ignoreList was added

--- js/glotdict-bulk.js
+++ js/glotdict-bulk.js
@@ -6,10 +6,21 @@ jQuery('.bulk-action').append(jQuery('<option>', {
 
 jQuery('.bulk-actions').on('click', '.button', function(e) {
   if (jQuery('.bulk-action option:selected').val() === 'copy-from-original') {
-    jQuery('th.checkbox input:checked').each(function() {
-      var parent = jQuery(this);
-      parent = parent.parent().parent();
-      var text = parent.find('.original').text().trim();
+      jQuery('th.checkbox input:checked').each(function() {
+
+      // List of selectors to ignore
+      var ignoreList = [
+        // any valid selector
+        '.context'
+      ];
+      var parent = jQuery(this).closest('tr');
+      
+      // Copy element which will be modified
+      var workingElement = parent.find('.original').clone();
+        
+      // Remove all the elements we don't need
+      workingElement.find(ignoreList.join()).remove();
+      var text = workingElement.text().trim();
       var row = parent.attr('row');
       jQuery('#editor-' + row + ' textarea').val(text);
     });
