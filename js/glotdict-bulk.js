72cb9219919abaf448f6982ac94b4b98621aea95
Karen <kaskar2008@mail.ru>
2018-10-05 13:01:51 +0300

    ignoreList was added (#196)
    * Fix #192 
    * Minor refactoring

--- js/glotdict-bulk.js
+++ js/glotdict-bulk.js
@@ -9,9 +9,20 @@ jQuery('.bulk-actions').on('click', '.button', function(e) {
     var copied_count = 0;
     $gp.editor.hide(); // Avoid validation on open editors that are empty.
     jQuery('th.checkbox input:checked').each(function() {
-      var parent = jQuery(this);
-      parent = parent.parent().parent();
-      var text = parent.find('.original').text().trim();
+
+      // List of selectors to ignore
+      var ignoreList = [
+        // any valid selector
+        '.context'
+      ];
+      var parent = jQuery(this).closest('tr');
+      
+      // Copy element which will be modified
+      var workingElement = parent.find('.original').clone();
+        
+      // Remove all the elements we don't need
+      workingElement.find(ignoreList.join()).remove();
+      var text = workingElement.text().trim();
       var row = parent.attr('row');
       jQuery('#editor-' + row + ' textarea').val(text);
       jQuery('#preview-' + row).addClass('has-original-copy');
