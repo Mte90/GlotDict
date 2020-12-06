19fbb00be2e77848e6f2f6c51b3d2f84849999a7
Webaxones <contact@webaxones.com>
2020-12-06 13:18:59 +0100

    fix-257-button-click

--- js/glotdict-column.js
+++ js/glotdict-column.js
@@ -9,66 +9,25 @@ function gd_add_column() {
   });
 }
 
-function gl_auto_hide_after_status() {
-   var check_if = setTimeout(function() {
-     if(jQuery('.editor:visible').length > 0) {
-           $gp.editor.hide();
-           clearTimeout(check_if);
-     }
-   }, 800);
-}
-
-jQuery('#translations').on('click', '.gd-approve', function() {
-  $gp.editor.show(jQuery(this));
-  $gp.editor.set_status(jQuery(this), 'current');
-  jQuery(this).parent().parent().removeClass('status-waiting').addClass('status-current');
-  gl_auto_hide_after_status();
-  return false;
-});
-jQuery('#translations').on('click', '.gd-reject', function() {
-  $gp.editor.show(jQuery(this));
-  $gp.editor.set_status(jQuery(this), 'rejected');
-  gl_auto_hide_after_status()
-  return false;
-});
-jQuery('#translations').on('click', '.gd-fuzzy', function() {
-  $gp.editor.show(jQuery(this));
-  $gp.editor.set_status(jQuery(this), 'fuzzy');
-  gl_auto_hide_after_status()
-  return false;
-});
-
 function gd_add_column_buttons(element) {
-  var approve = '';
-  var reject = '';
-  var fuzzy = '';
-  var approve_nonce = '';
-  var approve_title = '';
-  var reject_nonce = '';
-  var reject_title = '';
-  var fuzzy_nonce = '';
-  var fuzzy_title = '';
-  var id = jQuery(element).attr('row');
-
-  if (jQuery('#editor-' + id + ' .meta button.approve').length !== 0) {
-    approve_nonce = jQuery('#editor-' + id + ' .meta button.approve').attr('data-nonce');
-    approve_title = jQuery('#editor-' + id + ' .meta button.approve').attr('title');
-    approve = '<button class="approve gd-approve" tabindex="-1" data-nonce="' + approve_nonce + '" title="' + approve_title + '"><strong>+</strong> Approve</button>';
-  }
-  if (jQuery('#editor-' + id + ' .meta button.reject').length !== 0) {
-    reject_nonce = jQuery('#editor-' + id + ' .meta button.reject').attr('data-nonce');
-    reject_title = jQuery('#editor-' + id + ' .meta button.reject').attr('title');
-    reject = '<button class="reject gd-reject" tabindex="-1" data-nonce="' + reject_nonce + '" title="' + reject_title + '"><strong>−</strong> Reject</button>';
-  }
-  if (jQuery('#editor-' + id + ' .meta button.fuzzy').length !== 0) {
-    fuzzy_nonce = jQuery('#editor-' + id + ' .meta button.fuzzy').attr('data-nonce');
-    fuzzy_title = jQuery('#editor-' + id + ' .meta button.fuzzy').attr('title');
-    fuzzy = '<button class="fuzzy gd-fuzzy" tabindex="-1" data-nonce="' + fuzzy_nonce + '" title="' + fuzzy_title + '"><strong>~</strong> Fuzzy</button>';
-  }
-  var buttons = approve + reject + fuzzy;
-
-  if (jQuery(element).hasClass('untranslated')) {
-    buttons = '';
-  }
-  jQuery(element).append('<td>' + buttons + '</td>');
+  var td_buttons = document.createElement('TD');
+  element.append(td_buttons);
+  element.nextElementSibling.querySelectorAll('.meta button').forEach(function(button) {
+    var clone_button = button.cloneNode(true);
+    clone_button.classList.add('gd-' + clone_button.classList[0]);
+    clone_button.addEventListener('click', function(e) {
+      var button = e.target;
+      var editor = button.closest('tr.preview').nextElementSibling;
+      var new_status = button.classList[0];
+      new_status = new_status === 'approve' ? 'current' : new_status;
+      new_status = new_status === 'reject' ? 'rejected' : new_status;
+      $gp.editor.show(jQuery(button));
+      $gp.editor.set_status(jQuery(button), new_status);
+      editor.style.display = 'none';
+      $gp.editor.show(jQuery(button));
+    });
+    if (!element.classList.contains('untranslated')) {
+      td_buttons.append(clone_button);
+    }
+  });
 }
