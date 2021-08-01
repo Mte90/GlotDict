52eaa9fe7a25190caacabfe0cd67239d9e84136a
vlad-timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-08-01 15:00:44 +0300

    Consistency Suggestions
    Shows Consistency translations directly in the editor panel.
    
    Demo: https://user-images.githubusercontent.com/65488419/127769117-d027dca5-7526-4c7a-ac73-c496dadc03fb.gif
    
    Tests:
    - Loading suggestions
    - Copy to editor
    - Remember details state
    
    Tested browsers: Microsoft Edge 92.0.902.62, Google Chrome 92.0.4515.107, Firefox 90.0.2, Opera 77.0.4054.277, Brave 1.27.109

--- js/glotdict-consistency.js
+++ js/glotdict-consistency.js
@@ -2,6 +2,7 @@ if ( typeof $gp !== 'undefined' ){
   var gd_quicklinks_copy_state = localStorage.getItem( 'gd_quicklinks_copy_state' ) === 'true';
   var gd_quicklinks_window = { 'closed': true };
   gd_quicklinks();
+  gd_consistency();
 }
 
 function gd_quicklinks(){
@@ -91,4 +92,69 @@ function gd_toggle_quicklinks_copy() {
   } );
   gd_quicklinks_copy_state = ! gd_quicklinks_copy_state;
   localStorage.setItem( 'gd_quicklinks_copy_state', gd_quicklinks_copy_state );
+}
+
+function gd_consistency(){
+  var gd_consistency_output = createElement( 'details', { 'class': 'gd_consistency suggestions__translation-consistency', 'open': 'open' } );
+  var gd_consistency_summary = createElement( 'summary', { }, 'Suggestions from Consistency' );
+  var gd_consistency_button = createElement( 'button', { 'class': 'gd_get_consistency' }, 'View Consistency suggestions' );
+  gd_consistency_output.append( gd_consistency_summary, gd_consistency_button );
+
+  addElements( '.editor-panel__left .suggestions-wrapper', 'beforeend', gd_consistency_output );
+  addEvtListener( 'click', '.gd_get_consistency', gd_show_consistency );
+}
+
+function gd_show_consistency( event ) {
+  event.target.textContent = 'Loading...';
+  var consistency_url = event.target.closest( '.editor-panel' ).querySelectorAll( '.button-menu__dropdown li a' )[ 2 ].href;
+
+  fetch( consistency_url )
+    .then( consistency_response => consistency_response.text() )
+    .then( consistency_response => {
+      var consistency_parser = new DOMParser();
+      var consistency_page = consistency_parser.parseFromString( consistency_response , 'text/html' );
+      var consistency_translations = consistency_page.querySelectorAll( '.consistency-table tbody th strong' );
+      var translations_count, unique_translation_count;
+
+      if ( consistency_translations.length == 1 ) {
+        unique_translation_count = consistency_page.querySelectorAll( 'tr' ).length - 2;
+        unique_translation_count = ' ('+ unique_translation_count + ' time' + ( ( unique_translation_count > 1 ) ? 's' : '' ) + ')';
+      } else {
+          translations_count = consistency_page.querySelectorAll( '.translations-unique small' );
+      }
+
+      var gd_consistency_suggestions;
+
+      if ( consistency_translations.length ) {
+        var gd_consistency_item,
+        gd_consistency_item_div,
+        gd_consistency_item_translation,
+        gd_consistency_item_index,
+        gd_consistency_item_count,
+        gd_consistency_item_raw,
+        gd_consistency_item_button;
+
+        gd_consistency_suggestions = createElement( 'ul', { class: 'suggestions-list' } );
+
+        for ( var i = 0; i < consistency_translations.length; i++ ) {
+          gd_consistency_item = document.createElement( 'li' );
+          gd_consistency_item_div = createElement( 'div', { 'class': 'translation-suggestion with-tooltip', 'role': 'button', 'aria-pressed': 'false', 'aria-label': 'Copy translation', 'tabindex': '0' } );
+          gd_consistency_item_translation = createElement( 'span', { 'class': 'translation-suggestion__translation' }, consistency_translations[ i ].textContent );
+          gd_consistency_item_index = createElement( 'span', { 'class': 'translation-suggestion__translation index' }, ( i + 1 ) + ': ' );
+          gd_consistency_item_count = createElement( 'span', { 'class': 'consistency-count' }, ( consistency_translations.length == 1 ) ? unique_translation_count : translations_count[ i ].textContent );
+          gd_consistency_item_raw = createElement( 'span', { 'class': 'translation-suggestion__translation-raw', 'aria-hidden': 'true' }, consistency_translations[ i ].textContent );
+          gd_consistency_item_button = createElement( 'button', { 'type': 'button', 'class': 'copy-suggestion' }, 'Copy' );
+          gd_consistency_item_translation.prepend( gd_consistency_item_index );
+          gd_consistency_item_translation.append( gd_consistency_item_count );
+          gd_consistency_item_div.append( gd_consistency_item_translation, gd_consistency_item_raw, gd_consistency_item_button );
+          gd_consistency_item.append( gd_consistency_item_div );
+          gd_consistency_suggestions.append( gd_consistency_item );
+        }
+      } else {
+          gd_consistency_suggestions = 'No suggestions.';
+      }
+      event.target.before ( gd_consistency_suggestions );
+      event.target.parentNode.removeChild( event.target );
+    } )
+    .catch( error => console.log( error ) );
 }
