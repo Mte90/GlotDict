5f3a3faa637953ac46ba8c1248c9cf63cbfbd011
Loïc Antignac <contact@webaxones.com>
2021-09-20 14:56:39 +0200

    improve h2 styles (#337)

--- js/glotdict-functions.js
+++ js/glotdict-functions.js
@@ -99,7 +99,7 @@ function gd_add_project_links() {
 	if ( jQuery( '.gp-content .breadcrumb li' ).length > 3 && jQuery( '.gp-content .breadcrumb li:last-child a' ).length > 0 ) {
 		let lang = jQuery( '.gp-content .breadcrumb li:last-child a' ).attr( 'href' ).split( '/' );
 		lang = sanitize_value( lang[lang.length - 3] );
-		jQuery( jQuery( '.gp-content h2' )[0] ).prepend( `<a class="glossary-link" style="float:right;padding-left:5px;margin-left:5px;border-left: 1px solid black;" href="https://translate.wordpress.org/locale/${lang}/default" target="_blank" rel="noreferrer noopener">${jQuery( '.gp-content .breadcrumb li:last-child a' ).text()} Projects to Translate</a>` + '<a class="glossary-link" style="float:right;" href="https://translate.wordpress.org/stats" target="_blank" rel="noreferrer noopener">Translation Global Status</a>' );
+		jQuery( jQuery( '.gp-content h2' )[0] ).prepend( `<a class="glossary-link" style="float:right;" href="https://translate.wordpress.org/locale/${lang}/default" target="_blank" rel="noreferrer noopener">${jQuery( '.gp-content .breadcrumb li:last-child a' ).text()} Projects to Translate</a>` + '<a class="glossary-link" style="float:right;" href="https://translate.wordpress.org/stats" target="_blank" rel="noreferrer noopener">Translation Global Status</a>' );
 	}
 }
 
