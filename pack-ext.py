67a9207638cdd74fa7e90c59820297d1fa1bfa6e
Daniele Scasciafratte <mte90net@gmail.com>
2022-11-18 18:48:57 +0100

    feat(pack-ext): support for mv3

--- pack-ext.py
+++ pack-ext.py
@@ -9,7 +9,7 @@ print("Browser Extension packager by Mte90")
 print("The only parameter required is the folder path!")
 
 
-def zipdir(path, name):
+def zipdir(path, name, browser):
     zipf = zipfile.ZipFile(name, 'w', zipfile.ZIP_DEFLATED)
     exclude_prefixes = ['__', '.', 'eslint', 'tests']  # list of exclusion prefixes
     exclude_suffixes = ['.xpi', '.zip', '.py', 'ISSUE_TEMPLATE.md', 'README.md']  # list of exclusion suffix
@@ -36,8 +36,25 @@ if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
         with open(manifest) as content:
             data = json.load(content)
             name = data['name'].replace(' ', '-') + '_v' + data['version']
-            zipdir(sys.argv[1], name + '.zip')
-            print("- Package done!")
+            zipdir(sys.argv[1], name + '.xpi', 'firefox')
+            print("- Firefox WebExtension Package done!")
+            # convert manifest v2 to v3
+            os.system('cp ' + sys.argv[1] + '/manifest.json ' + sys.argv[1] + '/__manifest.json ')
+            # change parameters
+            data['background']['service_worker'] = data['background']['scripts'][0]
+            del data['background']['scripts']
+            data['resources'] = data['web_accessible_resources']
+            del data['web_accessible_resources']
+            data['manifest_version'] = 3
+
+            with open(manifest, 'w') as new_manifest:
+                json.dump(data, new_manifest, indent=4)
+            zipdir(sys.argv[1], name + '.zip', 'chrome')
+
+            # restore the original manifest
+            os.system('rm ' + sys.argv[1] + '/manifest.json')
+            os.system('mv ' + sys.argv[1] + '/__manifest.json ' + sys.argv[1] + '/manifest.json')
+            print("- Chrome Extension Package done!")
     else:
         print("The file" + manifest + " not exist")
         sys.exit()
